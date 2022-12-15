8ffd01a932c207363d9431ebdf8c8d94d6ff5435
Rukkyaa <lbrt.axel@gmail.com>
2022-12-15 11:40:45 +0100

    Test with readline

--- minishell.h
+++ minishell.h
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rukkyaa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/15 11:08:08 by rukkyaa           #+#    #+#             */
-/*   Updated: 2022/12/15 11:33:41 by rukkyaa          ###   ########.fr       */
+/*   Created: 2022/12/15 11:15:05 by rukkyaa           #+#    #+#             */
+/*   Updated: 2022/12/15 11:22:05 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-int	main(int argc, char **argv, char **envp)
-{
-	char	*line;
-
-	while (line = readline("Minishell> "))
-		printf("Line : %s\n", line);
-	return (EXIT_SUCCESS);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
