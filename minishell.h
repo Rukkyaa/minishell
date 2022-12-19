c9c43bdf10672ce495b165b9566bda57914b7c40
Dr-Gatsby <gatsby.dupre@gmail.com>
2022-12-19 12:19:40 +0100

    Premiers pas

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: gabrielduhau <gabrielduhau@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 11:15:05 by rukkyaa           #+#    #+#             */
-/*   Updated: 2022/12/19 11:03:55 by gabrielduha      ###   ########.fr       */
+/*   Updated: 2022/12/19 12:19:32 by gabrielduha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,11 @@
 # include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+char	**ft_split(char *s, char c);
+void	free_tab(char **tab);
+int	count_words(char const *str, char c);
+int	words_length(char const *str, int i, char c);
+
 
 #endif
